399058a21732c03e40e89cc7af8e050845e83dc5
Andrew Kozlik <akozlik@gmail.com>
2013-08-08 19:07:37 -0400

    Initial Commit

---
+++ SaltyBet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SaltyBet
+//
+//  Created by Andrew Kozlik on 8/8/13.
+//  Copyright (c) 2013 Andrew Kozlik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
